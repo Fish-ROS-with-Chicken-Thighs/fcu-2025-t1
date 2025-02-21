1477f2ad1886fb0d26081e04ec1c802a8cd7d58b
Hiromichi123 <2271612727@qq.com>
2025-02-21 15:46:18 +0800

    update

--- vision/setup.py
+++ vision/setup.py
@@ -26,7 +26,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'vision = 2025_1th.1th:main',
+            'vision = 2025_1th.vision:main',
         ],
     },
 )
